42b2328e3c66700ec173c0c435e16ff5897b0f96
Ibran Perez <iperez@odin.cs.csubak.edu>
2024-01-31 20:17:06 -0800

    lab02 changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+// Ibran Perez
 // CMPS 3350 - lab 02
 // Prints each command-line argument passed into the program, one per line
 #include <stdio.h>
 
 int main(int argc, char ** argv)
 {
-    for(int i = 1; i < argc; i++) {
+    for(int i = argc - 1; i > 0; i--) {
         printf("arg %d: %s\n", i, argv[i]);
     }
     return 0;
