a931bba90db4ed635e16e64b36511fb5d9750ddd
Ibran Perez <iperez@odin.cs.csubak.edu>
2024-01-31 20:29:52 -0800

    perpetuated the conficlt in main.cpp. oh no!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // Ibran Perez
-// CMPS 3350 - lab 02
+// CMPS 3350 - lab 02 is all about source control
 // Prints each command-line argument passed into the program, one per line
 #include <stdio.h>
 
